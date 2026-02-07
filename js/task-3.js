9326fa6754a79f54f243113753a68eef4528f831
emelgüneşli <emelgunesli@outlook.com>
2026-02-07 13:50:39 +0300

    correction

--- js/task-3.js
+++ js/task-3.js
@@ -8,7 +8,6 @@ console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"),
 console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px")));
 console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px")));
 
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+//console.log(getElementWidth("50px", "8px", "4px")); // 74
+//console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+//console.log(getElementWidth("200px", "0px", "0px")); // 200
