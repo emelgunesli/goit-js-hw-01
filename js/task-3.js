a575ae92b7380d9959f82872ef8bb18e43689499
emelgüneşli <emelgunesli@outlook.com>
2026-02-07 14:10:04 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,10 +4,10 @@ function getElementWidth(content, padding, border) {
     return toplamGenişlik;
 
 }
-console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px")));
-console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px")));
-console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px")));
+//console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px")));
+//console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px")));
+//console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px")));
 
-//console.log(getElementWidth("50px", "8px", "4px")); // 74
-//console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-//console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px"))); // 74
+console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px"))); // 101
+console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px"))); // 200
